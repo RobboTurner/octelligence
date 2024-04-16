d99a762fdce1d83d04e03f5d883e46b79bae98e9
Ubuntu <ubuntu@ip-172-31-43-239.eu-west-2.compute.internal>
2024-04-16 10:27:00 +0000

    Add some basic tts stuff

--- tts/tts.py
+++ tts/tts.py
@@ -1,11 +1,32 @@
 from openai import OpenAI
+import pygame
+from dotenv import load_dotenv
+import os
+os.environ['SDL_AUDIODRIVER'] = 'pulse'
+pygame.mixer.init(frequency=22050, size=-16, channels=2)
 
-client = OpenAI()
 
-response = client.audio.speech.create(
-    model="tts-1",
-    voice="alloy",
-    input="Hello world! This is a streaming test.",
-)
+# Load environment variables from .env file
+load_dotenv()
 
-response.stream_to_file("output.mp3")
+def tts(content: str):
+    client = OpenAI()
+
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=content,
+    )
+
+    filename = "output.mp3"
+    response.stream_to_file(filename)
+
+    # Initialize pygame mixer
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
+
+    # Wait for the music to play
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+tts('Hello world! This is a text to speech test.')
